823c0172a85103194e9caf6810e47ab979a5dd91
Yan Gao <yangao@thoughtworks.com>
2015-02-03 01:50:14 +0800

    [gy] add some pics and main contents.

--- js/a.js
+++ js/a.js
@@ -8,4 +8,19 @@ $('.background').mousemove(function(event) {
     $(this).css({
         "background-position" : "" + left + "% " + top + "%"
     });
-})
+});
+
+var prevTop = 0, currTop = 0;
+$(window).scroll(function() {
+    currTop = $(window).scrollTop() + 1;
+    var nav = $('#top-nav');
+    if (currTop > 120) {
+        if (!nav.hasClass('scrolled')) nav.removeClass('nav-down').addClass('nav-up').addClass('scrolled');
+    }
+    else
+        nav.removeClass('nav-up').removeClass('scrolled').addClass('nav-down');
+
+    if(currTop < prevTop && nav.hasClass('scrolled')) nav.removeClass('nav-up').addClass('nav-down');
+    if(nav.hasClass('scrolled') && nav.hasClass('nav-down') && currTop >= prevTop) nav.removeClass('nav-down').addClass('nav-up');
+    setTimeout(function(){prevTop = currTop},0);
+});
